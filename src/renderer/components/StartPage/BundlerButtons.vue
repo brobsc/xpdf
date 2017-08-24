192a45d0c8f5414f65a7bc6643dcb197b0506fb2
Bruno <bruno.robsiq@gmail.com>
2017-08-24 12:08:11 -0300

    Move pdf manipulation to lib

--- src/renderer/components/StartPage/BundlerButtons.vue
+++ src/renderer/components/StartPage/BundlerButtons.vue
@@ -8,10 +8,14 @@
 </template>
 
 <script>
-  import execa from 'execa';
+  import tools from '../../lib/PDFTools.js';
 
   export default {
-    name: 'bundler-buttons',
+    data() {
+      return {
+        name: 'bundler-buttons',
+      };
+    },
 
     methods: {
       showPDF: true,
@@ -27,51 +31,11 @@
         const files = this.$store.state.files;
         const images = [];
 
-        files.forEach(file => images.push(this.resizeImage(file)));
+        files.forEach(file => images.push(tools.optimize(file)));
 
-        // pdf.save('ops.pdf');
-        this.convertToPDF(images);
+        tools.convert(images, 90);
       },
-
-      resizeImage(file) {
-        const newPath = `/Users/bruno/Desktop/${file.name}`;
-
-        execa.shellSync(`gm convert -size 595x842 '${file.path}'\
-          -background white\
-          -gravity center\
-          -rotate '90>'\
-          -geometry '595x842>'\
-          -extent 595x842\
-          -quality 90\
-          '${newPath}'`);
-
-        return newPath;
-      },
-
-      getImageSize(file) {
-        const command = execa.shellSync(`gm identify -format "%w %h" '${file}'`);
-        const result = command.stdout.split(' ');
-
-        return result;
-      },
-
-      /* eslint-disable */
-      convertToPDF(images) {
-        // Converts a /path/to/image to "'/path/to/image' "
-        // and joins them to get rid of ',' imposed by JS arrays
-        const allPaths = images.map(image => `'${image}' ` ).join("");
-        const command = `gm convert\
-          ${allPaths}\
-          -contrast -contrast\
-          -compress JPEG\
-          -quality 40\
-          /Users/bruno/Desktop/man.pdf`;
-
-        execa.shellSync(command);
-      },
-      /* eslint-enable */
     },
-
   };
 </script>
 
---
+++ src/renderer/lib/PDFTools.js
@@ -0,0 +1,45 @@
+import execa from 'execa';
+
+export default {
+  optimize(file, quality = 90) {
+    // TODO: Make temp path
+    const newPath = `/Users/bruno/Desktop/${file.name}`;
+
+    execa.shellSync(`gm convert -size 595x842 '${file.path}'\
+          -background white\
+          -gravity center\
+          -rotate '90>'\
+          -geometry '595x842>'\
+          -extent 595x842\
+          -quality ${quality}\
+          '${newPath}'`);
+
+    return newPath;
+  },
+
+  convert(images, quality = 40, contrast = true) {
+    // TODO: Use temp path
+    // Converts a /path/to/image to "'/path/to/image' "
+    // and joins them to get rid of ',' imposed by JS arrays
+    let contrastString = '';
+    const allPaths = images.map(image => `'${image}' `).join('');
+
+    if (contrast) contrastString = '-contrast -contrast';
+
+    const command = `gm convert\
+          ${allPaths}\
+          ${contrastString}\
+          -compress JPEG\
+          -quality ${quality}\
+          /Users/bruno/Desktop/temp.pdf`;
+
+    execa.shellSync(command);
+  },
+
+  getSize(file) {
+    const command = execa.shellSync(`gm identify -format "%w %h" '${file}'`);
+    const result = command.stdout.split(' ');
+
+    return result;
+  },
+};
